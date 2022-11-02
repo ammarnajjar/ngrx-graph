4bd7a453ac109e0c70dff47e5369c9841c0f2e37
Ammar Najjar <najjarammar@gmail.com>
2022-11-02 21:37:26 +0100

    check for mapNode to be CallExpression

--- src/index.ts
+++ src/index.ts
@@ -43,30 +43,32 @@ function effectDispatchedActions(
     "mergeMap",
   ]);
   let actions = [];
-  mapNodes.forEach(mapNode => {
-    actions = [
-      ...actions,
-      ...allActions.filter(action => mapNode.getText().includes(action)),
-    ];
-    ts.forEachChild(mapNode.arguments[0], (node: any) => {
-      if (node.kind === SyntaxKind.CallExpression && node.expression.name) {
-        const privateMethodName = node.expression.name.escapedText.toString();
-        const callables = getParentNodes(sourceFile, privateMethodName);
-        callables.forEach(callable => {
-          if (callable.kind === SyntaxKind.PropertyDeclaration) {
-            actions = [
-              ...actions,
-              ...allActions.filter(action =>
-                callable.getText().includes(action)
-              ),
-            ];
-          }
-        });
-      }
+  mapNodes
+    .filter(node => node.kind === SyntaxKind.CallExpression)
+    .forEach(mapNode => {
+      actions = [
+        ...actions,
+        ...allActions.filter(action => mapNode.getText().includes(action)),
+      ];
+      ts.forEachChild(mapNode.arguments[0], (node: any) => {
+        if (node.kind === SyntaxKind.CallExpression && node.expression.name) {
+          const privateMethodName = node.expression.name.escapedText.toString();
+          const callables = getParentNodes(sourceFile, privateMethodName);
+          callables.forEach(callable => {
+            if (callable.kind === SyntaxKind.PropertyDeclaration) {
+              actions = [
+                ...actions,
+                ...allActions.filter(action =>
+                  callable.getText().includes(action)
+                ),
+              ];
+            }
+          });
+        }
+      });
     });
-  });
 
-  return [...new Set(actions.filter(action => !input.includes(action)))]
+  return [...new Set(actions.filter(action => !input.includes(action)))];
 }
 
 function getEffectActionsMap(sourceFile: any) {
@@ -81,7 +83,6 @@ function getEffectActionsMap(sourceFile: any) {
   });
 }
 
-
 function extract(file: string, identifiers: string[]): void {
   const options: ts.CompilerOptions = { allowJs: true };
   const compilerHost = ts.createCompilerHost(
@@ -91,12 +92,13 @@ function extract(file: string, identifiers: string[]): void {
   let program = ts.createProgram([file], options, compilerHost);
   const sourceFile = program.getSourceFile(file);
   const effectActionsMap = getEffectActionsMap(sourceFile);
-  console.dir(effectActionsMap, { depth: null })
+  console.dir(effectActionsMap, { depth: null });
 }
 
 glob("**/assets/*.effects.ts", function (err, files) {
   files.forEach(filename => {
     const fullFileName = join(__dirname, "../", filename);
+    console.log("🚀 ~ fullFileName", fullFileName);
     extract(fullFileName, []);
   });
 });
