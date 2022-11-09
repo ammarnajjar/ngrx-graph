522f75ee81a8b1cc10c1c069183a36513bf6556d
Ammar Najjar <najjarammar@gmail.com>
2022-11-09 21:58:51 +0100

    add jsonOnly flag and more description to flags/args

--- src/commands/graph/index.ts
+++ src/commands/graph/index.ts
@@ -7,20 +7,51 @@ export default class Graph extends Command {
   static examples = ['<%= config.bin %> <%= command.id %>'];
 
   static flags = {
-    force: Flags.boolean({ char: 'f' }),
-    all: Flags.boolean({ char: 'a' }),
-    srcDir: Flags.string({ char: 'd', default: process.cwd() }),
-    outputDir: Flags.string({ char: 'o', default: '/tmp' }),
-    structureFile: Flags.string({ char: 's', default: 'ngrx-graph.json' }),
+    force: Flags.boolean({
+      char: 'f',
+      description: 'Force regenrating the graph structure',
+    }),
+    jsonOnly: Flags.boolean({
+      char: 'j',
+      description:
+        'Generate only the structure json file, can be combined with --structureFile option. It overrides --all and [ACTION]',
+    }),
+    all: Flags.boolean({
+      char: 'a',
+      description:
+        'Generate the whole graph for all actions and connected component, effects and reducers. It will be ignored if --jsonOnly is used',
+    }),
+    srcDir: Flags.string({
+      char: 'd',
+      default: process.cwd(),
+      description:
+        'Source directory to grab actions from, usually the directory with package.json in it',
+    }),
+    outputDir: Flags.string({
+      char: 'o',
+      default: '/tmp',
+      description: 'Destination directory, where to save the generated files',
+    }),
+    structureFile: Flags.string({
+      char: 's',
+      default: 'ngrx-graph.json',
+      description:
+        'Then name of the structure json file, Path is taken from --outputDir option',
+    }),
   };
 
   static args = [
-    { name: 'action', description: 'Action of interest', required: false },
+    {
+      name: 'action',
+      description:
+        'Action of interest. It will be ignored if --jsonOnly is used',
+      required: false,
+    },
   ];
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Graph);
-    const { all, srcDir, outputDir, structureFile, force } = flags;
+    const { all, srcDir, outputDir, structureFile, force, jsonOnly } = flags;
     const { action } = args;
 
     CliUx.ux.action.start('Collecting all actions');
@@ -43,32 +74,34 @@ export default class Graph extends Command {
     gen.saveStructure(fromComponents, fromEffects, fromReducers);
     CliUx.ux.action.stop();
 
-    if (action) {
-      CliUx.ux.action.start(` ⚡️ ${action} `);
-      gen.generateActionGraph(
-        action,
-        fromComponents,
-        fromEffects,
-        fromReducers,
-      );
-      CliUx.ux.action.stop();
-    } else {
-      for (const _action of gen.allActions) {
-        CliUx.ux.action.start(` ⚡️ ${_action} `);
+    if (!jsonOnly) {
+      if (action) {
+        CliUx.ux.action.start(` ⚡️ ${action} `);
         gen.generateActionGraph(
-          _action,
+          action,
           fromComponents,
           fromEffects,
           fromReducers,
         );
         CliUx.ux.action.stop();
+      } else {
+        for (const _action of gen.allActions) {
+          CliUx.ux.action.start(` ⚡️ ${_action} `);
+          gen.generateActionGraph(
+            _action,
+            fromComponents,
+            fromEffects,
+            fromReducers,
+          );
+          CliUx.ux.action.stop();
+        }
       }
-    }
 
-    if (all) {
-      CliUx.ux.action.start('Generating the complete graph');
-      gen.generateAllGraph(fromComponents, fromEffects, fromReducers);
-      CliUx.ux.action.stop();
+      if (all) {
+        CliUx.ux.action.start('Generating the complete graph');
+        gen.generateAllGraph(fromComponents, fromEffects, fromReducers);
+        CliUx.ux.action.stop();
+      }
     }
   }
 }
