1d5e38fb05706fb20d12eaac78827632daab7889
Ammar Najjar <najjarammar@gmail.com>
2025-11-12 16:51:42 +0100

    refactor effects in case2: improve formatting and remove lodash import

--- docs/examples/case2/src/case2.effects.ts
+++ docs/examples/case2/src/case2.effects.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
 import { createEffect, ofType } from '@ngrx/effects';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { action1, action2, action3, nestedAction1, nestedAction2 } from './actions';
-import { map } from 'lodash';
 
 @Injectable()
 export class ExampleEffects {
   effect1$ = createEffect(() =>
     this.actions$.pipe(
       ofType(action1),
-      switchMap(() => [nestedAction1({ action: action2() }), action3()])),
+      switchMap(() => [nestedAction1({ action: action2() }), action3()]),
     ),
   );
 
   effect2$ = createEffect(() =>
     this.actions$.pipe(
       ofType(nestedAction1),
-      map(({ action }) => nestedAction2( { action: action()})),
+      map(({ action }) => nestedAction2({ action: action() })),
     ),
   );
 
   effect3$ = createEffect(() =>
     this.actions$.pipe(
       ofType(nestedAction2),
-      map(({ action }) => action())),
+      map(({ action }) => action()),
     ),
   );
 }
