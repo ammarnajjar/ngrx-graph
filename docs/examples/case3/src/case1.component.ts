fa775add241324a74af74a24ff461c5c0dd54128
Ammar Najjar <najjarammar@gmail.com>
2025-11-13 11:52:27 +0100

    delete unused action and example files; add new actions, component, effects, and reducer for case3

---
+++ docs/examples/case3/src/case1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { action1 } from './actions';
+
+@Component({ selector: 'first-cmp', template: '' })
+export class FirstComponent {
+  constructor(private store: Store) {}
+
+  onEvent() {
+    this.store.dispatch(action1());
+  }
+}
---
+++ docs/examples/case3/src/case1.effects.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { switchMap, tap } from 'rxjs/operators';
+import { action1, action2, action3 } from './actions';
+
+@Injectable()
+export class FirstEffects {
+  effect1$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(action1),
+      switchMap(() => [action2()]),
+    ),
+
+    this.actions$.pipe(
+      ofType(action3),
+      tap(console.log),
+    ),
+  );
+
+  constructor(private actions$: Actions) {}
+}
